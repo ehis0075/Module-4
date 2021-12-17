4e5d9c23627d639e512e7fde57ddadcca418dfff
ehis0075 <ehisjude420@gmail.com>
2021-12-17 13:44:57 +0100

    first push

---
+++ org/paumard/Main.java
@@ -0,0 +1,17 @@
+package org.paumard;
+
+
+
+import java.util.function.Function;
+
+/**
+ *
+ * @author José Paumard
+ */
+public class Main {
+
+    public static void main(String... args) {
+        
+
+    }
+}
