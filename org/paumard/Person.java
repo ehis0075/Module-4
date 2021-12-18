d3c16715de5dfe62f57d30a664532bde4d5e9f1f
ehis0075 <ehisjude420@gmail.com>
2021-12-18 10:18:59 +0100

    Map, reduce, filter

--- org/paumard/Person.java
+++ org/paumard/Person.java
@@ -1,40 +1,30 @@
 package org.paumard;
 
 
-
 /**
  *
  * @author ehis
  */
+
 public class Person {
 
-    private String firstName;
-    private String lastName;
+    private String name;
     private int age;
 
     public Person() {
     }
 
-    public Person(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Person(String name, int age) {
+        this.name = name;
         this.age = age;
     }
-    
-    public String getFirstName() {
-        return firstName;
+
+    public String getName() {
+        return name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
@@ -47,6 +37,7 @@ public class Person {
 
     @Override
     public String toString() {
-        return "Person{" + "firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + '}';
+        return "Person{" + "name=" + name + ", age=" + age + '}';
     }
 }
+
